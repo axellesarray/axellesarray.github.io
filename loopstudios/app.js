fef5ad7d7d67d1900b3d3ac588fd66339d03ae84
axellesarray <axelle.sarray@gmail.com>
2021-03-15 11:48:45 +0100

    Update app.js

--- loopstudios/app.js
+++ loopstudios/app.js
@@ -1,3 +1,4 @@
+const body = document.querySelector("body");
 const burger = document.querySelector(".hamburger-icon");
 const close = document.querySelector(".close-icon");
 const nav = document.querySelector(".header nav");
@@ -6,11 +7,13 @@ burger.addEventListener("click", () => {
   nav.classList.add("nav__show");
   nav.classList.remove("nav__hide");
   nav.classList.remove("nav");
+  body.classList.remove("relative");
 });
 
 close.addEventListener("click", () => {
   nav.classList.remove("nav__show");
   nav.classList.add("nav__hide");
+  body.classList.add("relative");
 
   setTimeout(function () {
     nav.classList.add("nav");
